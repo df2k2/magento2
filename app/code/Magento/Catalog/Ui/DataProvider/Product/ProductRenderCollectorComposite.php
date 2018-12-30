0bb137538969304caaa9affc20d12c5a57902fca
Chris Snedaker <df2002@gmail.com>
2018-12-29 23:59:27 -0500

    Removed redundant property initialization, overridden immediately in constructor; Added strict types declaration

--- app/code/Magento/Catalog/Ui/DataProvider/Product/ProductRenderCollectorComposite.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/ProductRenderCollectorComposite.php
@@ -3,6 +3,9 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+declare(strict_types=1);
+
 namespace Magento\Catalog\Ui\DataProvider\Product;
 
 use Magento\Catalog\Api\Data\ProductInterface;
@@ -17,7 +20,7 @@ class ProductRenderCollectorComposite implements ProductRenderCollectorInterface
     /**
      * @var ProductRenderCollectorInterface[]
      */
-    private $productProviders = [];
+    private $productProviders;
 
     /**
      * ProductRenderCollectorComposite constructor.
